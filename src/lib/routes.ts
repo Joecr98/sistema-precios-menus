9656727861498ac3d8e660ff9397943653e752b8
Henry Najarro <hnajarrog@miumg.edu.gt>
2024-10-21 16:09:23 -0600

    se agrega archivo y ruta de configuracón de rutas de los modulos

---
+++ src/lib/routes.ts
@@ -0,0 +1,11 @@
+export const routes = {
+    login: '/',
+    menu: '/menu',
+    roles: '/roles',
+    areas: '/areas',
+    usuarios: '/usuarios',
+    facturacion: '/facturacion',
+    menus: '/menus',
+    productos: '/productos',
+  };
+  
